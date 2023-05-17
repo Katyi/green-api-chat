5eabb2497dda26ac55a558a719b7eabf0bb0c954
Alexandra Egorova <egorova.aleksandra@gmail.com>
2023-05-17 20:08:09 +0200

    added some code

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -3,12 +3,11 @@ import {useForm} from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
-  const { register, handleSubmit, reset, watch, formState: { errors } } = useForm();
+  const { register, handleSubmit, reset, formState: { errors } } = useForm();
   const navigate = useNavigate();
 
   const loginUser = data => {
     console.log(data)
-    localStorage.setItem("username", data.username);
     localStorage.setItem("idInstance", data.idInstance);
     localStorage.setItem("apiTokenInstance", data.apiTokenInstance);
     reset();
@@ -17,23 +16,12 @@ const Login = () => {
 
   return (
     <div className='container'>
-      <h1>Авторизация пользователя</h1>
+      <h1>Ввести учетные данные GREEN-API</h1>
       <form onSubmit={handleSubmit(loginUser)} className='LoginForm'>
-        <div className='loginFormDiv'>
-          <label>Имя пользователя: </label>
-          <input 
-            type="text" {...register("username", 
-            {
-              required: "Поле обязательно к заполнению",
-              minLength: {value: 2, message: "Минимум 2 символa"},
-              maxLength: {value: 40, message: "Максимум 40 символов"},
-            })} 
-          />
-          {errors.username && <span>{errors.username.message}</span>}
-        </div>
         <div className='loginFormDiv'>
           <label>idInstance: </label>
           <input 
+            autoComplete='off'
             type="number" {...register("idInstance", 
             {
               required: "Поле обязательно к заполнению",
@@ -43,7 +31,8 @@ const Login = () => {
         </div>
         <div className='loginFormDiv'>
           <label>apiTokenInstance: </label>
-          <input 
+          <input
+            autoComplete='off' 
             type="text" {...register("apiTokenInstance", 
             {
               required: "Поле обязательно к заполнению",
--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -1,22 +1,30 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {useForm} from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 
 const Main = () => {
-  let username = localStorage.getItem("username");
+  const navigate = useNavigate();
   let idInstance = localStorage.getItem("idInstance");
   let apiTokenInstance = localStorage.getItem("apiTokenInstance");
   const [showPhoneNumber, setShowPhoneNumber] = useState(false);
   const [showInput, setShowInput] = useState(false);
   const [phoneNumber, setPhoneNumber] = useState("");
+  const [phoneNumberList, setPhoneNumberList] = useState([]);
   const { register, handleSubmit, reset, formState: { errors } } = useForm();
   const { register: register2, handleSubmit: handleSubmit2, reset: reset2, formState: { errors: errors2 }} = useForm();
   let [messages, setMessages] = useState([]);
-  let i = 0;
+  let ind = 0;
 
-  const beginChat = data => {
+  const startChat = data => {
     setShowInput(false);
     setShowPhoneNumber(true);
     setPhoneNumber(data.phone);
+    setPhoneNumberList((prev)=>[...prev, data.phone]);
+    reset();
+  };
+
+  const changeChat = (e) => {
+    setPhoneNumber((prev)=>e.target.innerHTML);
   };
 
   const sendMessage = data => {
@@ -41,18 +49,16 @@ const Main = () => {
         console.log(result)
         if (result) {
           console.log("done!")
-          setMessages((prev)=>[...prev, {typeOfMessage: "out", message: data.message}]);
+          setMessages((prev)=>[...prev, {typeOfMessage: "out", message: data.message, receiver: phoneNumber}]);
           console.log(messages);
         }
       })
-
       .catch(error => console.log('error', error));      
       reset2();
-
   };
 
-  const receiveMessage = (i) => {
-    i = i + 1;
+  const receiveMessage = (ind) => {
+    ind = ind + 1;
     let requestOptions = {
       method: 'GET',
     };
@@ -60,27 +66,37 @@ const Main = () => {
     fetch(`https://api.green-api.com/waInstance${idInstance}/receiveNotification/${apiTokenInstance}`, requestOptions)
       .then(response => response.text())
       .then(result => {
-         console.log('result '+ result);
-         console.log('result '+ typeof result);
         if (result === "null") {
-          console.log("выход")
-          throw new Error("Ошибка");
+          throw new Error("Сообщений нет");
+        }
+        let receiptId = JSON.parse(result).receiptId;
+        console.log(receiptId);
+        if (JSON.parse(result).body.typeWebhook !== 'incomingMessageReceived') {
+          deletereceivedMessage(receiptId);
+          throw new Error("Не того типа сообщение!!!");
         }
         let resMessage;
-        let receiptId;
+        let resSender;
         if (JSON.parse(result).body.messageData.typeMessage === 'textMessage') {
-            resMessage = JSON.parse(result).body.messageData.textMessageData.textMessage;
-            receiptId = JSON.parse(result).receiptId;
-            setMessages((prev)=>[...prev, {typeOfMessage: "input", message: resMessage}]);
-            console.log(messages);
-            console.log(receiptId);
-            deletereceivedMessage(receiptId);
-          } else {
-            throw new Error("Ошибка");
-          };
+          resMessage = JSON.parse(result).body.messageData.textMessageData.textMessage;
+          resSender = JSON.parse(result).body.senderData.sender.slice(0, 11);
+          setMessages((prev)=>[...prev, {typeOfMessage: "input", message: resMessage, sender: resSender}]);
+          console.log(messages);
+          console.log(resSender);
+          deletereceivedMessage(receiptId);
+        } else if (JSON.parse(result).body.messageData.typeMessage === 'extendedTextMessage') {
+          resMessage = JSON.parse(result).body.messageData.extendedTextMessageData.text;
+          resSender = JSON.parse(result).body.senderData.sender.slice(0, 11);
+          setMessages((prev)=>[...prev, {typeOfMessage: "input", message: resMessage, sender: resSender}]);
+          console.log(messages);
+          console.log(resSender);
+          deletereceivedMessage(receiptId);
+        } else {
+          deletereceivedMessage(receiptId);
+          throw new Error("Не текстовое сообщение");
+        };
       })
-      .catch(error => console.log('error', error));
-      
+      .catch(error => console.log(error));
   };
 
   const deletereceivedMessage = (receiptId) => {
@@ -92,74 +108,92 @@ const Main = () => {
     fetch(`https://api.green-api.com/waInstance${idInstance}/deleteNotification/${apiTokenInstance}/${receiptId}`, requestOptions)
       .then(response => response.text())
       .then(result => console.log(result))
-      .catch(error => console.log('error', error));
+      .catch(error => console.log(error));
   };
   
   useEffect(() => {
-    // setTimeout(console.log(i), 20000);
-    setInterval(() => receiveMessage(i), 20000);
-    
-  },[i])
+    setInterval(() => receiveMessage(ind), 20000);
+  },[ind])
 
   return (
     <div className='container'>
       {/* Header */}
       <div className='header'>
-        <img src="/images/arrow_back.png" alt="" className='back-arrow-img' />
-        <div className='usernameInHeader'>{username}</div>
-        {!showInput && phoneNumber ==="" &&
+        <div className='headerLeftSideIcons'>  
+        <img src="/images/arrow_back.png" className='back-arrow-img' onClick={()=>navigate('/')}/>
+        {!showInput &&
           <button className='chat-icon-btn' onClick={()=>setShowInput(true)}>
-            <img src="/images/chat-icon.png" alt="" className='chat-icon-img' />
+            <div className='tooltip'>
+            <img src="/images/chat-icon.png" alt="" className='chat-icon-img'/>
+            <span className="tooltiptext">Новый чат</span>
+            </div>
           </button>
         }
         {showInput &&
-        <form key={1} onSubmit={handleSubmit(beginChat)} className='LoginForm'>
-          <div>
-            <label>Введите номер телефона: </label>
-            <input
-              className='phoneNumberInput'
-              placeholder="79269999999"
-              type="number" {...register("phone",
-              {
-                required: "Поле обязательно к заполнению",
-                minLength: {value: 11, message: "Минимум 11 символa"},
-                maxLength: {value: 11, message: "Максимум 11 символов"},
-              })} 
-            />
-            {errors.phone && <span>{errors.phone.message}</span>}
-            <button className='mainFormButton'>Ok</button>
-          </div>
+        <form key={1} onSubmit={handleSubmit(startChat)} className='phoneForm'>
+          <label>Введите номер телефона: </label>
+          <input
+            className='phoneNumberInput'
+            placeholder="79269999999"
+            autoComplete='off'
+            type="number" {...register("phone",
+            {
+              required: "Поле не заполнено",
+              minLength: {value: 11, message: "11 символов"},
+              maxLength: {value: 11, message: "11 символов"},
+              validate: value=> !phoneNumberList.includes(value) || "Такой номер уже есть в списке!"
+            })} 
+          />
+          <br />
+          {errors.phone && <span>{errors.phone.message}</span>}
+          <button className='chat-icon-btn'>
+            <img src="/images/ok-icon.png" alt="" className='chat-icon-img ok-icon'/>
+          </button>
          </form>
         }
+        </div>
         {showPhoneNumber &&
-          <div className='phoneNumber'>to {phoneNumber}</div>
+          <div className='phoneNumber'>{phoneNumber}</div>
         }
       </div>
-
-      {/* Messages show part */}
-      <div className='messages'>
-        {messages.map((item, index)=>{
-          return <div 
-            key={index} 
-            className={item.typeOfMessage === 'out' ? 'outMessage' : 'inputMessage'}
+      {/* main part */}
+      <div className='main'>
+        {/* sidebar */}
+        <div className='sidebar'>
+          <h3>Контакты</h3>
+          {phoneNumberList.map((item, index)=> {
+            return <div key={index} className='contact'
+            onClick={(e)=>changeChat(e)}
             >
-              {item.typeOfMessage === 'out' ? item.message : item.message}
+              {item}
             </div>
-          })
-        }
+          })}
+        </div>
+        {/* Messages part */}
+        <div className='messages'>
+          {messages.filter((item, index)=> (item.sender === phoneNumber && item.typeOfMessage === 'input') || (item.receiver === phoneNumber && item.typeOfMessage === 'out'))
+          .map((item, index)=>{
+            return <div 
+              key={index} 
+              className={item.typeOfMessage === 'out' ? 'outMessage' : 'inputMessage'}
+              >
+                {item.message}
+              </div>
+            })
+          }
+        </div>
       </div>
-
+      
       {/* Footer for sending messages */}
       <div className='footer'>
         <form key={2} onSubmit={handleSubmit2(sendMessage)} className='MessageForm'>
           <input
             placeholder='Ваше сообщение...'
             className='messageInput'
+            autoComplete='off'
             type="text" {...register2("message",
               {
-                required: "Поле обязательно к заполнению",
-                minLength: {value: 1, message: "Минимум 1 символa"},
-                maxLength: {value: 100, message: "Максимум 100 символов"},
+                required: "Поле не заполнено",
               })}
           />
           {errors2.message && <span>{errors2.message.message}</span>}
@@ -167,7 +201,6 @@ const Main = () => {
             <img src="/images/send-message.png" alt="" className='chat-icon-img' />
           </button>
         </form>
-
       </div>
     </div>
   )
